64d8672a6f493f540992671232668a2996b2c523
Константин <koty_166@mail.ru>
2024-11-30 17:24:40 +0300

    08.10.28

--- new_app.c
+++ new_app.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-	FILE *secret = fopen("/challenge/app-systeme/ch5/.passwd", "rt");
+	FILE *secret = fopen("challenge/app-systeme/ch5/.passwd", "rt");
 	char buffer[32];
 	fgets(buffer, sizeof(buffer), secret);
 	printf(argv[1]);
